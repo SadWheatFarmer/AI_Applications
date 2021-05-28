e33ac0f81a81d57aff8215c94d779ba0ca48b0ae
SadWheatFarmer <smutnyjw@gmail.com>
2021-05-27 23:08:52 -0400

    IBM_LanguageTranslator: Added German Translator. All Unit Tests passing. Need to improve sytex.

--- IBM Applied AI/Python for AI & App Development/Project-IBM_LanguageTranslator/machinetranslation/README.txt
+++ IBM Applied AI/Python for AI & App Development/Project-IBM_LanguageTranslator/machinetranslation/README.txt
@@ -1,3 +1,6 @@
+IBM Watson Language Translator
+
+
 WARNING
 Be careful about how you are getting your translations. If you rely on Google
 Translate to populate EXPECTED values for Unit Tests, there is a chance that it
@@ -5,6 +8,13 @@ will be wrong.
 Examples (IBM_Watson / Google Translate)
 A) "Joy To the World" = joy au monde / joie au monde
 B) "Peace to the Earth" = Paix sur Terre / paix sur la terre
+C) "Blue Square\nRed Circle" = cercle rouge bleu carre / carre bleu cercle rouge
+    - 3 Errors...
+        1) For IBM translation, the new line char is gone.
+        2) For IBM translation, the words are flipped from order given
+                (red circle blue square) vs (blue square red circle)
+        3) For IBM translation, the 'red circle' french words are flipped.
+                The IBM version is saying 'circle red'
 
 WARNING
 Watson IBM translate API can only handle 1 line at a time. It will remove '\n'
--- IBM Applied AI/Python for AI & App Development/Project-IBM_LanguageTranslator/machinetranslation/translator.py
+++ IBM Applied AI/Python for AI & App Development/Project-IBM_LanguageTranslator/machinetranslation/translator.py
@@ -1,36 +1,44 @@
+################################################################################
 """
-Be sure to install the 'ibm_watson' python package.
-From Terminal: pip install ibm_watson
+Author: John Smutny
+Date: 05/27/2021
+Des: Module that uses an IBM Watson Language Translator service to translate
+        text from English to French or German.
+
+NOTE: Be sure to install the 'ibm_watson' python package.
+        From Terminal: pip install ibm_watson
 """
+################################################################################
+
 
 # IBM imports to use Language Translator IBM
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson import LanguageTranslatorV3
 
-# Modules for language conversion
-from pandas import json_normalize
-
 # IBM API information
-languageAPI_Key = "CzyI_s7p_YCQXJFjTFkOWiOLzfziH-t1HyzScVh41fcN"
-languageAPI_URL = "https://api.us-south.language-translator.watson.cloud.ibm" \
-                  ".com/instances/5f48ed0d-4238-4607-bdfe-307366425776 "
+LANGUAGE_API_KEY = "CzyI_s7p_YCQXJFjTFkOWiOLzfziH-t1HyzScVh41fcN"
+LANGUAGE_API_URL1 = "https://api.us-south.language-translator.watson.cloud.ibm"
+LANGUAGE_API_URL2 = ".com/instances/5f48ed0d-4238-4607-bdfe-307366425776"
+LANGUAGE_API_URL = LANGUAGE_API_URL1 + LANGUAGE_API_URL2
 version_lt = '2018-05-01'
 
 
 def english_to_french(en_text):
     """
-
-    See this URL for IBM LanguageTranslator version information
-    - https://cloud.ibm.com/apidocs/language-translator?code=python
-    :return: 
+    Translate text from English to French using IBM Watson API.
+    :param en_text: English string to translate.
+    :return: String of text in French.
     """
-    authenticator = IAMAuthenticator(languageAPI_Key)
+
+    # Initialize the IBM Watson API
+    authenticator = IAMAuthenticator(LANGUAGE_API_KEY)
     language_translator = LanguageTranslatorV3(version=version_lt,
                                                authenticator=authenticator)
-    language_translator.set_service_url(languageAPI_URL)
+    language_translator.set_service_url(LANGUAGE_API_URL)
 
     # Prevent user from trying to translate null values.
-    if en_text != None:
+    if en_text != "":
+        # Translate text and output a formatted string
         translator_response = language_translator.translate(text=en_text,
                                                             model_id='en-fr')
         fr_translation = translator_response.get_result()['translations'][0][
@@ -41,3 +49,31 @@ def english_to_french(en_text):
         fr_translation = "NULL"
 
     return fr_translation
+
+
+def english_to_german(en_text):
+    """
+    Translate text from English to German using IBM Watson API.
+    :param en_text: English string to translate.
+    :return: String of text in German.
+    """
+
+    # Initialize the IBM Watson API
+    authenticator = IAMAuthenticator(LANGUAGE_API_KEY)
+    language_translator = LanguageTranslatorV3(version=version_lt,
+                                               authenticator=authenticator)
+    language_translator.set_service_url(LANGUAGE_API_URL)
+
+    # Prevent user from trying to translate null values.
+    if en_text != "":
+        # Translate text and output a formatted string
+        translator_response = language_translator.translate(text=en_text,
+                                                            model_id='en-de')
+        de_translation = translator_response.get_result()['translations'][0][
+            'translation']
+    else:
+        print("[ERROR] translator/english_to_german() - Cannot translate a "
+              "NULL string.")
+        de_translation = "NULL"
+
+    return de_translation
